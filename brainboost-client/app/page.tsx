413f24b86ee19a7b2d38ad5239d4cb33f8f17cd4
Krish Koria <krishkoria2004@gmail.com>
2024-12-01 19:28:46 +0530

    implement landing page component and remove Prettier configuration

--- brainboost-client/app/page.tsx
+++ brainboost-client/app/page.tsx
@@ -1,7 +1,11 @@
+import LandingPage from "@/components/LandingPage";
 import NonDashboardNavbar from "@/components/NonDashboardNavbar";
 
 export default function Home() {
   return <div className="nondashboard-layout">
     <NonDashboardNavbar />
-  </div>;
+    <main className="nondashboard-layout__main">
+      <LandingPage/>
+    </main>
+  </div>
 }
