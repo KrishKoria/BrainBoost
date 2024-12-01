2855946975a93221927628b4a6878537d192e31c
Krish Koria <krishkoria2004@gmail.com>
2024-12-01 18:04:31 +0530

    added notifications button

--- brainboost-client/components/NonDashboardNavbar.tsx
+++ brainboost-client/components/NonDashboardNavbar.tsx
@@ -1,11 +1,13 @@
-import { BookOpen } from 'lucide-react'
+import { Bell, BookOpen } from 'lucide-react'
 import Link from 'next/link'
 import React from 'react'
+import { Button } from './ui/button'
 
 const NonDashboardNavbar = () => {
   return (
     <nav className='nondashboard-navbar'>
         <div className="nondashboard-navbar__container">
+            <div className="nondashboard-navbar__search">
             <Link href="/" className='nondashboard-navbar__brand'>BrainBoost</Link>
             <div className="flex items-center gap-4">
                 <div className="relative group">
@@ -16,8 +18,14 @@ const NonDashboardNavbar = () => {
                  <BookOpen className='nondashboard-navbar__search-icon' size={18} />
                 </div>
             </div>
+            </div>
         </div>
-        
+        <div className="nondashboard-navbar__actions">
+            <Button className='nondashboard-navbar__notification-button'>
+            <span className='nondashboard-navbar__notification-indicator'></span>
+            <Bell className='nondashboard-navbar__notification-icon'/>
+            </Button>
+        </div>
     </nav>
   )
 }
