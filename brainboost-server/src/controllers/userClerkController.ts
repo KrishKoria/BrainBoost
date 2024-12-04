3284b4a10b0517960c02853ecdecbc7619dbb83d
Krish Koria <krishkoria2004@gmail.com>
2024-12-04 14:31:37 +0530

    feat: implement user notification settings component and integrate with user API

--- brainboost-server/src/controllers/userClerkController.ts
+++ brainboost-server/src/controllers/userClerkController.ts
@@ -8,13 +8,13 @@ export const updateUser = async (
   try {
     const userId = req.params.userId;
     const userData = req.body;
-    await clerk.users.updateUserMetadata(userId, {
+    const user = await clerk.users.updateUserMetadata(userId, {
       publicMetadata: {
         userType: userData.publicMetadata.userType,
         settings: userData.publicMetadata.settings,
       },
     });
-    res.status(200).json({ message: "User Updated Successfully" });
+    res.status(200).json({ message: "User Updated Successfully", data: user });
   } catch (error) {
     res.status(500).json({ message: "Error Updating User", error: error });
   }
