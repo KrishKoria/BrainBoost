ec403d6265172b40ef405418ee7f6bac5d7cdf51
Krish Koria <krishkoria2004@gmail.com>
2024-12-05 23:23:16 +0530

    feat: add listTransactions endpoint to retrieve user transactions

--- brainboost-server/src/routes/transactionRoutes.ts
+++ brainboost-server/src/routes/transactionRoutes.ts
@@ -2,9 +2,11 @@ import express from "express";
 import {
   createStripePaymentIntent,
   createTransaction,
+  listTransactions,
 } from "../controllers/transactionController";
 const router = express.Router();
 
+router.get("/", listTransactions);
 router.post("/stripe/payment-intent", createStripePaymentIntent);
 router.post("/", createTransaction);
 
