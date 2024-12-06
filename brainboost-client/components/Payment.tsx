253782e1af39c0c753b32f292410ba4e97979c43
Krish Koria <krishkoria2004@gmail.com>
2024-12-06 18:07:19 +0530

    feat: update return_url in Payment component to use dynamic base URL for Stripe redirection

--- brainboost-client/components/Payment.tsx
+++ brainboost-client/components/Payment.tsx
@@ -29,10 +29,16 @@ const PaymentContent = () => {
       toast.error("Stripe service is not available");
       return;
     }
+    const baseUrl = process.env.NEXT_PUBLIC_LOCAL_URL
+      ? `http://${process.env.NEXT_PUBLIC_LOCAL_URL}`
+      : process.env.NEXT_PUBLIC_VERCEL_URL
+        ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+        : undefined;
+
     const result = await stripe.confirmPayment({
       elements,
       confirmParams: {
-        return_url: `${process.env.NEXT_PUBLIC_STRIPE_REDIRECT_URL}/checkout?step=3&courseId=${courseId}`,
+        return_url: `${baseUrl}/checkout?step=3&courseId=${courseId}`,
       },
       redirect: "if_required",
     });
